70137a5d203d7433008a8009c1fdc451dc057251
Jonathan ES Lin <ernsheong@gmail.com>
2017-11-13 23:16:44 +0800

    Initial attempt

--- boilertext/shallow_text.go
+++ boilertext/shallow_text.go
@@ -1,19 +1,24 @@
 package boilertext
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/pkg/errors"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 type TextBlock struct {
 	NumOfWords           int
 	NumOfAnchorTextWords int
+	Content              []byte
 }
 
-func (t TextBlock) LinkDensity() float64 {
+func (t *TextBlock) LinkDensity() float64 {
 	if t.NumOfWords != 0 && t.NumOfAnchorTextWords != 0 {
 		return float64(t.NumOfAnchorTextWords) / float64(t.NumOfWords)
 	}
@@ -33,12 +38,53 @@ func (s ShallowTextExtractor) Process(reader io.Reader) ([]byte, error) {
 		return nil, errors.Wrap(err, "Parse HTML error")
 	}
 
+	blocks := make([]*TextBlock, 0, 20)
+	var bufferText bytes.Buffer
+	var bufferAnchorText bytes.Buffer
+
 	var f func(n *html.Node)
 	f = func(n *html.Node) {
-		if n.Type == html.ElementNode {
-			fmt.Println("Element Node", "DataAtom", n.DataAtom, "Data", n.Data)
-		} else if n.Type == html.TextNode {
-			fmt.Println("Text Node", "DataAtom", n.DataAtom, "Data", n.Data)
+		if n.Type == html.TextNode {
+			fmt.Println("Text Node:", "Parent", n.Parent.DataAtom, "Data", n.Data, "NextSibling", n.NextSibling)
+			switch n.Parent.DataAtom {
+			case atom.A:
+				bufferText.WriteString(strings.TrimSpace(n.Data))
+				bufferAnchorText.WriteString(strings.TrimSpace(n.Data))
+			case atom.Strike, atom.U, atom.B, atom.I, atom.Em, atom.Strong, atom.Span, atom.Sup, atom.Code, atom.Tt, atom.Sub, atom.Var, atom.Font:
+				bufferText.WriteString(strings.TrimSpace(n.Data))
+			case atom.Style, atom.Script, atom.Option, atom.Object, atom.Embed, atom.Applet, atom.Link, atom.Noscript:
+				// Ignore
+			default:
+				// Generate a new block
+				bufferText.WriteString(strings.TrimSpace(n.Data))
+
+				var copyBufferText bytes.Buffer
+				io.TeeReader(&bufferText, &copyBufferText)
+
+				textScanner := bufio.NewScanner(&bufferText)
+				// Set the split function for the scanning operation.
+				textScanner.Split(bufio.ScanWords)
+				// Count the words.
+				textCount := 0
+				for textScanner.Scan() {
+					textCount++
+				}
+
+				anchorTextScanner := bufio.NewScanner(&bufferAnchorText)
+				// Set the split function for the scanning operation.
+				anchorTextScanner.Split(bufio.ScanWords)
+				// Count the words.
+				anchorTextCount := 0
+				for anchorTextScanner.Scan() {
+					anchorTextCount++
+				}
+
+				blocks = append(blocks, &TextBlock{
+					NumOfWords:           textCount,
+					NumOfAnchorTextWords: anchorTextCount,
+					Content:              copyBufferText.Bytes(),
+				})
+			}
 		}
 
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
@@ -48,5 +94,58 @@ func (s ShallowTextExtractor) Process(reader io.Reader) ([]byte, error) {
 
 	f(node)
 
-	return []byte(""), nil
+	// Block processing complete. Let's gather the wheat and discard the chaff.
+
+	var contentText bytes.Buffer
+	var prev, next *TextBlock
+	for i, curr := range blocks {
+		if i == 0 {
+			prev = nil
+		} else {
+			prev = blocks[i-1]
+		}
+		if i == len(blocks)-1 {
+			next = nil
+		} else {
+			next = blocks[i+1]
+		}
+
+		isContent := false
+		if curr.LinkDensity() <= 0.333333 {
+			if prev != nil && prev.LinkDensity() <= 0.555556 {
+				if curr.NumOfWords <= 16 {
+					if next != nil && next.NumOfWords <= 15 {
+						if prev != nil && prev.NumOfWords <= 4 {
+							isContent = false
+						} else {
+							isContent = true
+						}
+					} else {
+						isContent = true
+					}
+				} else {
+					isContent = true
+				}
+			} else {
+				if curr.NumOfWords <= 40 {
+					if next != nil && next.NumOfWords <= 17 {
+						isContent = false
+					} else {
+						isContent = true
+					}
+				} else {
+					isContent = true
+				}
+			}
+		} else {
+			isContent = false
+		}
+
+		if isContent {
+			contentText.Write(curr.Content)
+			contentText.WriteString(" ")
+		}
+	}
+
+	return contentText.Bytes(), nil
 }
