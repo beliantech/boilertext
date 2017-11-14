96b304940d353cefe232df4c2f801be4669039b5
Jonathan ES Lin <ernsheong@gmail.com>
2017-11-14 14:51:36 +0800

    Initial benchmark

---
+++ boilertext/shallowtext_test.go
@@ -0,0 +1,23 @@
+package boilertext
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"testing"
+)
+
+func BenchmarkShallowText(b *testing.B) {
+	extractor := NewShallowTextExtractor(bufio.ScanWords)
+	file, err := os.Open("../sample/nyt.html")
+	if err != nil {
+		log.Fatal("Failed to open file")
+	}
+
+	for n := 0; n < b.N; n++ {
+		_, err := extractor.Process(file)
+		if err != nil {
+			log.Fatal("Extraction failed")
+		}
+	}
+}
